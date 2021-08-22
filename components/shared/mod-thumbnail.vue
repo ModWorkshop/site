7de8dd2bc283f97d2482b9a33bb21db198ffc54f
Luffy <luffydafloffi@gmail.com>
2021-08-22 09:09:50 +0300

    Fixed thumbnails

--- components/shared/mod-thumbnail.vue
+++ components/shared/mod-thumbnail.vue
@@ -1,8 +1,8 @@
 <template>
     <picture class="card-img-top overflow-hidden">
-        <source :data-srcset="mod.thumbnail" :type="mod.thumbnail_type == 'webp' && 'image/webp'">
-        <source data-srcset="/mws/assets/images/nopreview.png" type="image/png">
-        <img src="http://localhost:8001/storage/assets/nopreview.png" :class="{lazy: true, 'ratio-image': true, nsfw: mod.is_nsfw && !mod.uses_default_thumbnail}" alt="thumbnail">
+        <source v-if="mod.thumbnail" :srcset="mod.thumbnail" :type="mod.thumbnail_type == 'webp' && 'image/webp'">
+        <source srcset="http://localhost:8001/storage/assets/nopreview.png" type="image/png">
+        <img src="http://localhost:8001/storage/assets/nopreview.png" :class="{'ratio-image': true, nsfw: mod.is_nsfw && !mod.uses_default_thumbnail}" alt="thumbnail">
     </picture>
 </template>
 <script>
