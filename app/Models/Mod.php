528c2f94c5c01528d655d45e14952a785c90fa86
Luffy <luffydafloffi@gmail.com>
2021-08-22 10:49:12 +0300

    Link game to mod

--- app/Models/Mod.php
+++ app/Models/Mod.php
@@ -15,14 +15,21 @@ class Mod extends Model
      * @var array
      */
     protected $guarded = [];
+    protected $appends = ['submitter', 'game'];
 
     public function submitter() {
         return $this->hasOne(User::class, "id", 'submitter_uid')->latest();
     }
 
-    protected $appends = ['submitter'];
+    public function game() {
+        return $this->hasOne(Category::class, "id", 'game_id')->latest();
+    }
 
     public function getSubmitterAttribute() {
         return $this->submitter()->first();
+    }
+
+    public function getGameAttribute() {
+        return $this->game()->first();
     }
 }
