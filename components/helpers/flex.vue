19bb2e877a9d131e3a72803c686a5f4deec8813b
Luffy <luffydafloffi@gmail.com>
2021-08-23 05:31:06 +0300

    Let flex component be inline

--- components/helpers/flex.vue
+++ components/helpers/flex.vue
@@ -1,9 +1,10 @@
 <template>
-    <div :class="{flex: true, 'flex-col': column, 'flex-wrap': wrap, [`gap-${gap}`]: !!gap, 'flex-grow': grow}"><slot/></div>
+    <div :class="{flex: !inline, 'inline-flex': inline, 'flex-col': column, 'flex-wrap': wrap, [`gap-${gap}`]: !!gap, 'flex-grow': grow}"><slot/></div>
 </template>
 <script>
     export default {
         props: {
+            inline: Boolean,
             column: Boolean,
             wrap: Boolean,
             gap: [Number, String],
