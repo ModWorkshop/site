9e6b98ab8491fb2cede9915c20f23c1c04441d20
Luffy <luffydafloffi@gmail.com>
2021-09-07 15:45:57 +0300

    Some SSR fixes for Axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,10 +44,7 @@ export default {
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
-		{
-			src: "~/plugins/axios.js",
-			mode: "client"
-		},
+		"@/plugins/axios.js",
 		"@/plugins/element-ui",
 		"@/plugins/factory",
 		"@/plugins/fa"
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,12 @@
 export default function({$axios, app}) {
+    // Set XSRF token for SSR
+    const token = app.$cookies.get('XSRF-TOKEN');
+    $axios.setHeader('X-XSRF-TOKEN', token);
+
+    if (process.server) {
+        return;
+    }
+
     function check() {
         if (!app.$cookies.get('XSRF-TOKEN')) {
             console.log("Looks like the token has expired...");
