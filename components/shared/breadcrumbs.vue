f8bd1594a5559b579466a95c38482532240db025
Luffy <luffydafloffi@gmail.com>
2021-09-09 22:09:31 +0300

    Fix breadcrumb

--- components/shared/breadcrumbs.vue
+++ components/shared/breadcrumbs.vue
@@ -1,7 +1,7 @@
 <template>
     <flex gap="2" class="breadcrumbs">
-        <template v-for="(item, i) in items">
-            <nuxt-link v-if="item.to" :key="item.name" :to="item.to">{{item.name}}</nuxt-link>
+        <template v-for="(item, i) in crumbs">
+            <nuxt-link v-if="item.href && i != items.length - 1" :key="item.name" :to="item.href">{{item.name}}</nuxt-link>
             <span v-else :key="item.name">{{item.name}}</span>
             <span v-if="i != items.length - 1" :key="`split_${item.name}`"> / </span>
         </template>
@@ -12,6 +12,11 @@
     export default {
         props: {
             items: Array
+        },
+        computed: {
+            crumbs() {
+                return this.items.slice().reverse();
+            }
         }
     }
 </script>
