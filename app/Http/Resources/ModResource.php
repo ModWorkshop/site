d5329d0cf8483c3178683a1a8945e9c1a47ee600
Luffy <luffydafloffi@gmail.com>
2021-09-22 03:57:12 +0300

    Create resources

---
+++ app/Http/Resources/ModResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\File;
+use Arr;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+/**
+ * Since we don't want to give the API something like download_type = App\Models\File
+ * we just convert it
+ */
+const downloadTypeSimple = [
+    File::class => 'file',
+];
+
+class ModResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return array_merge(parent::toArray($request), [
+            'files' => $this->files,
+            'images' => $this->images,
+            'tags' => TagResource::collection($this->whenLoaded('tags')),
+            'download' => $this->whenLoaded('files', function() {
+                if ($this->download_type === File::class) {
+                    return $this->files->find($this->download_id);
+                }
+            }),
+            'download_type' => downloadTypeSimple[$this->download_type] ?? null,
+        ]);
+    }
+}
