93d1165ce36e2422fbd134646844821508548ceb
Luffy <luffydafloffi@gmail.com>
2021-10-04 06:33:42 +0300

    Policy for roles

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
+    public function __construct() {
+        $this->authorizeResource(Role::class, 'role');
+    }
+
     /**
      * Display a listing of the resource.
      *
