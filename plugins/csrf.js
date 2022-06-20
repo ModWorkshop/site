27d8c1a6e61e3a81a6a52221fa59c1c85d4d25f2
Luffy <luffydafloffi@gmail.com>
2022-06-20 23:36:04 +0300

    Return CSRF check

--- plugins/csrf.js
+++ plugins/csrf.js
@@ -1,9 +1,26 @@
 import { $fetch } from 'ohmyfetch';
 export default defineNuxtPlugin(nuxtApp => {
-    // Set XSRF token for SSR
-    const headers = useRequestHeaders(['cookie']);
     const token = useCookie('XSRF-TOKEN');
-    const ftch = $fetch.create({ baseURL: 'http://localhost:8000', headers: {Referer: 'localhost:3000', 'X-XSRF-TOKEN': token.value} });
+    const opt = { baseURL: 'http://localhost:8000', credentials: "include", headers: {Referer: 'localhost:3000', 'X-XSRF-TOKEN': token.value} };
+
+    if (process.client) {
+        // Set XSRF token for SSR
+        async function check() {
+            if (!useCookie('XSRF-TOKEN').value) {
+                console.log("Fetching CSRF token. " );
+                await $fetch('http://localhost:8000/sanctum/csrf-cookie', {
+                    credentials: "include"
+                });
+    
+                opt['X-XSRF-TOKEN'] = useCookie('XSRF-TOKEN').value;
+            }
+        }
+        setInterval(check, 30000); // Check every 5 minutes
+        check();
+    }
+
+    //Just a new instance of fetch that links to our API
+    const ftch = $fetch.create(opt);
 
     return {
         provide: {
