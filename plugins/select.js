1d5e9db1d1defad3481c5282ff1174d50b0263c3
Luffy <luffydafloffi@gmail.com>
2022-06-21 04:57:17 +0300

    Fixed popper breaking build of site

--- plugins/select.js
+++ plugins/select.js
@@ -1,6 +1,6 @@
 import VueEasyLightbox from 'vue-easy-lightbox';
 import VueObserveVisibility from 'vue3-observe-visibility2';
-import Popper from "vue3-popper";
+import Popper from 'vue3-popper/dist/popper.esm';
 
 export default defineNuxtPlugin(nuxtApp => {
     // Register the component globally
