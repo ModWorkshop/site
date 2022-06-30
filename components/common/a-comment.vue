921a6928ff99f128116aa546b943e7dbf31234e4
Luffy <luffydafloffi@gmail.com>
2022-06-30 20:21:41 +0300

    Adjust design for comments

--- components/common/a-comment.vue
+++ components/common/a-comment.vue
@@ -1,29 +1,24 @@
 <template>
     <content-block :alt-background="isReply" :gap="null" :class="{comment: true, reply: isReply, focus: currentFocus && currentFocus.id == comment.id}" :id="`comment-cid${comment.id}`">
-        <flex class="comment-body p-2">
-            <div class="mr-2 my-auto" >
+        <flex class="comment-body p-1">
+            <div class="mr-1">
                 <nuxt-link :to="`/user/${comment.user_id}`">
                     <a-avatar :src="`http://localhost:8000/storage/${comment.user.avatar}`" size="medium"/>
                 </nuxt-link>
             </div>
             <flex column wrap class="overflow-hidden w-full mt-2">
-                <flex gap="1" :key="updateKey">
+                <flex :key="updateKey">
                     <a-user :avatar="false" :user="comment.user"/>
                     <span v-if="comment.special_type" class="text-success">({{comment.special_type}})</span>
-                    <a class="text-body" :title="comment.created_at" :href="`/post/${comment.id}`">{{timeAgo(comment.created_at)}}</a>
-                    <span v-if="comment.updated_at != comment.created_at" :title="comment.updated_at">({{$t('edited')}})</span>
+                    <a class="ml-1 text-secondary" :title="comment.created_at" :href="`/post/${comment.id}`">{{timeAgo(comment.created_at)}}</a>
+                    <span v-if="comment.updated_at != comment.created_at" class="text-secondary" :title="comment.updated_at">({{$t('edited')}})</span>
                     <font-awesome-icon v-if="comment.pinned" class="transform rotate-45" icon="thumbtack" :title="$t('pinned')"/>
                 </flex>
-                <form v-if="comment.canedit" method="post" class="hidden_form p-2 d-none" style="width:100%;">
-                    <!-- <textarea class="edited_message" name="edited_message" v-model="comment.content"/> -->
-                    <input type="submit" :value="$t('edit')" class="confirm_edit btn btn-primary mt-2"> 
-                    <button class="stop_editing btn btn-primary mt-2">{{$t('cancel')}}</button>
-                </form>
-                <div class="comment_message p-1 text-break markdown w-full">
+                <div class="comment-message my-2 text-break markdown w-full">
                     <markdown :text="comment.content"/>
                 </div>
             </flex>
-            <div class="float-right mt-3">
+            <div class="float-right">
                 <flex class="comment-actions" :style="{visibility: areActionsVisible ? 'visible' : null}">
                     <a v-if="canReply" class="reply-button text-body mr-1 cursor-pointer" title="Reply" role="button" @click="$emit('reply', comment)">
                         <font-awesome-icon icon="reply"/>
@@ -46,7 +41,7 @@
             </div>
         </flex>
         <template v-if="!isReply">
-            <flex column gap="1" class="replies px-6">
+            <flex column class="replies px-6">
                 <a-comment v-for="reply of comment.last_replies" 
                     :key="reply.id"
                     :data="reply"
