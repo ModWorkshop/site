12d682761fdd1a5bbdd6ccf745ab77f620408093
Luffy <luffydafloffi@gmail.com>
2022-07-19 23:00:36 +0300

    Change where images are stored

--- components/common/mod-thumbnail.vue
+++ components/common/mod-thumbnail.vue
@@ -1,6 +1,6 @@
 <template>
     <picture class="card-img-top overflow-hidden">
-        <source v-if="mod.thumbnail" :srcset="`http://localhost:8000/storage/images/${mod.thumbnail.file}`">
+        <source v-if="mod.thumbnail" :srcset="`http://localhost:8000/storage/mods/images/${mod.thumbnail.file}`">
         <source srcset="http://localhost:8000/storage/assets/nopreview.png" type="image/png">
         <img src="http://localhost:8000/storage/assets/nopreview.png" :class="{'ratio-image': true}" alt="thumbnail">
     </picture>
