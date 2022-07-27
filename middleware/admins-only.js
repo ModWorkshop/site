d20030f7519b7fcbb205f7ca50fb5233fed906d4
Luffy <luffydafloffi@gmail.com>
2022-07-27 11:25:14 +0300

    Fluff

--- middleware/admins-only.js
+++ middleware/admins-only.js
@@ -2,10 +2,8 @@ import { useStore } from '../store';
 
 export default defineNuxtRouteMiddleware((to) => {
     const { user, hasPermission } = useStore();
-    console.log('hello');
+    
     if (!user || !hasPermission('admin')) {
         throwError("You don't have permission to view this page");
-    } else {
-        console.log("you're fine");
     }
 });
