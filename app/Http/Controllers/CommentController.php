05d6ee68712b80b7c2e140b58bbfb4203ae751af
Luffy <luffydafloffi@gmail.com>
2022-07-27 11:19:10 +0300

    Policy for comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -14,6 +14,10 @@ use Illuminate\Http\Response;
 
 class CommentController extends Controller
 {
+    public function __construct() {
+        $this->authorizeResource(Comment::class, 'comment');
+    }
+
     /**
      * Display a listing of the resource.
      *
---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Comment;
+use App\Models\Mod;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Auth\Access\Response;
+
+class CommentPolicy
+{
+    use HandlesAuthorization, AuthorizesRequests;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(?User $user)
+    {
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(?User $user, Comment $comment)
+    {
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->hasPermission('edit-own-comment');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Comment $comment)
+    {
+        return ($user->hasPermission('edit-own-comment') && $comment->user->id === $user->id) || $user->hasPermission('edit-comment');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Comment $comment)
+    {
+        if ($comment->user->id === $user->id) {
+            return true;
+        } else {
+
+            
+            if ($comment->commentable instanceof Mod && $this->authorize('update', $comment->commentable)) {
+
+
+                return $user->hasPermission('delete-own-mod-comment');
+            } else {
+                return $user->hasPermission('delete-comment');
+            }
+        }
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Comment $comment)
+    {
+        //
+    }
+}
