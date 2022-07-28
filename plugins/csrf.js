c7b245905af50b989e4695e7da1c8e2906a93d03
Luffy <luffydafloffi@gmail.com>
2022-07-29 01:55:12 +0300

    Move CSRF token check function outside of the function
    to comply with ESLint

--- plugins/csrf.js
+++ plugins/csrf.js
@@ -1,13 +1,14 @@
 import { reloadToken } from '../utils/helpers';
-export default defineNuxtPlugin(nuxtApp => {
+async function check() {
+    if (!useCookie('XSRF-TOKEN').value) {
+        console.log("Fetching CSRF token. " );
+        reloadToken();
+    }
+}
+
+export default defineNuxtPlugin(() => {
     if (process.client) {
         // Set XSRF token for SSR
-        async function check() {
-            if (!useCookie('XSRF-TOKEN').value) {
-                console.log("Fetching CSRF token. " );
-                reloadToken();
-            }
-        }
         setInterval(check, 300000); // Check every 5 minutes
         check();
     }
