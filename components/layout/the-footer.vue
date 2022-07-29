88f56bf289d43a6c2d1778a7f78ec9508b6f6c33
Luffy <luffydafloffi@gmail.com>
2022-07-29 07:22:04 +0300

    Updated footer

--- components/layout/the-footer.vue
+++ components/layout/the-footer.vue
@@ -1,13 +1,17 @@
 <template>
     <footer>
-        MWS V3 - EARLY AS SHIT Alpha
+        ModWorkshop v3.0 Alpha
+        <br>
+        <span>
+            Made with ❤ by <nuxt-link to="/@Luffy">Luffy</nuxt-link>
+        </span>
     </footer>
 </template>
 <style>
     footer {
         padding: 1rem;
         display: flex;
-        align-items: center;
+        flex-direction: column;
         background-color: var(--header-footer-color);
         grid-area: footer;
     }
