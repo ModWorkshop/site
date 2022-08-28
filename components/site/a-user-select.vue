f472daa64fea7c5080bc384c16dedfa09ec231f8
Luffy <luffydafloffi@gmail.com>
2022-08-28 04:38:12 +0300

    Fixed user select not searching properly

--- components/site/a-user-select.vue
+++ components/site/a-user-select.vue
@@ -1,5 +1,7 @@
 <template>
-    <a-select v-model="modelValue" :options="users" text-by="text" @update-search="searchUsers" @update:model-value="value => $emit('update:modelValue', value)"/>
+    <div>
+        <a-select v-model="modelValue" :options="users" text-by="text" @update-search="searchUsers" @update:model-value="value => $emit('update:modelValue', value)"/>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -18,23 +20,29 @@ interface SearchUser extends User {
     text: string
 }
 
-const { data: paginatedUsers, refresh } = await useAsyncData('get-users', () => {
+const { data: paginatedUsers, refresh } = await useAsyncData(() => {
     let user_id;
 
     if (typeof(props.modelValue) == 'number') {
         user_id = props.modelValue;
     }
 
+    console.log('..');
+    
+
     return useGetMany<SearchUser>('users', { params: { query: query.value, user_id } });
 });
 
-async function searchUsers(q: string) {
+function searchUsers(q: string) {
     query.value = q;
+
+    console.log('...');
+    
 
     if (lastTimetout) {
         clearTimeout(lastTimetout);
     }
-    lastTimetout = setTimeout(() => refresh, 200);
+    lastTimetout = setTimeout(refresh, 200);
 }
 
 const users = computed(() => {
