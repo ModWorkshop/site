45cfd73c2eba0cddd5e0bf633a03be87d193eb2b
Luffy <luffydafloffi@gmail.com>
2022-08-29 08:17:42 +0300

    Improved login page

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,7 @@
         <a-form @submit="login">
             <flex column gap="3">
                 <a-input v-model="user.email" label="Email"/>
-                <a-input v-model="user.current_password" label="Password" type="password"/>
+                <a-input v-model="user.password" label="Password" type="password"/>
                 <flex column gap="2">
                     Or register using one the following
                     <flex>
@@ -13,45 +13,48 @@
                     </flex>
                 </flex>
                 <a-input v-model="user.remember" label="Remember Me" type="checkbox"/>
+                <a-alert v-if="error" color="danger" class="w-full">{{error}}</a-alert>
                 <div>
-                    <a-button type="submit" large>{{$t('login')}}</a-button>
+                    <a-button type="submit" :disabled="!canLogin">{{$t('login')}}</a-button>
                 </div>
             </flex>
         </a-form>    
     </page-block>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useStore } from '~~/store';
 
 definePageMeta({
     middleware: 'guests-only'
 });
 
-const user = ref({
+const user = reactive({
     email: '',
-    current_password: '',
+    password: '',
     remember: true,
 });
 
 const error = ref('');
+const canLogin = computed(() => user.email && user.password);
 const store = useStore();
 const { public: config } = useRuntimeConfig();
 
 async function login() {
     error.value = '';
     try {
-        await usePost('/login', user.value);
-    } catch (error) {
+        await usePost('/login', user);
+        store.attemptLoginUser();
+    } catch (e) {
         const codes = {
             401: 'Incorrect email or password',
             422: 'Given email or password are invalid'
         };
-        console.log(error);
-        //error.value = codes[error.response.status] || 'Something went wrong';
+        console.log(e.response);
+        console.log(codes[e.response.status] || 'Something went wrong');
+        
+        error.value = codes[e.response.status] || 'Something went wrong';
         return;
     }
-
-    store.attemptLoginUser();
 }
 </script>
