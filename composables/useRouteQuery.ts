6c36cc3d909674eaea042f86c15daded8dfa1f15
Luffy <luffydafloffi@gmail.com>
2022-08-29 03:15:40 +0300

    Fixed weirdness with useRouteQuery and number values

--- composables/useRouteQuery.ts
+++ composables/useRouteQuery.ts
@@ -1,15 +1,22 @@
 //This is only added due to VueUse's one breaking on Nuxt 3.
 
-export default function<T extends string | string[]>(name: string, defaultValue?: T) {
+export default function<T extends number | string | string[]>(name: string, defaultValue?: T, cast?: (data: string|string[]) => T) {
     const router = useRouter();
     const route = useRoute();
-    return computed<any>({
+    return computed({
         get() {
             const data = route.query[name];
-            if (data == null)
+            if (data == null) {
                 return defaultValue ?? null;
-            if (Array.isArray(data))
+            }
+            if (Array.isArray(data)) {
                 return data.filter(Boolean);
+            }
+            
+            if (cast) {
+                return cast(data);
+            }
+
             return data;
         },
         set(v) {
