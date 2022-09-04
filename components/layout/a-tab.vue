a26de6b3aa22e46dc1533276a9c3a68fc513299e
Luffy <luffydafloffi@gmail.com>
2022-09-04 03:40:30 +0300

    Change a-tab to flex

--- components/layout/a-tab.vue
+++ components/layout/a-tab.vue
@@ -1,32 +1,28 @@
 <template>
-    <div 
+    <flex 
         v-show="show"
-        :id="`${name}-tab-panel`" 
         role="tabpanel"
         :aria-hidden="!show"
         :aria-labelledby="`${name}-tab-link`"
         tabindex="0"
         class="tab-panel"
+        column
+        :gap="gap"
 	>
         <slot/>
-    </div>
+    </flex>
 </template>
 
 <script setup>
 const props = defineProps({
     name: String,
-    title: String
+    title: String,
+    gap: {
+        type: [String, Number],
+        default: 4
+    }
 });
 
 const tabState = inject('tabState');
 const show = computed(() => props.name == tabState.current);
 </script>
-
-<style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-}
-</style>
