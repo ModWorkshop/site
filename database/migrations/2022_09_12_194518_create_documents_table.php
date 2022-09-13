7e7a3c0865bd3be352dc987484535eb00b4deef3
Luffy <luffydafloffi@gmail.com>
2022-09-13 08:38:10 +0300

    Forgot migration for documents

---
+++ database/migrations/2022_09_12_194518_create_documents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+
+            $table->tinyText('name');
+            $table->tinyText('url_name');
+            $table->text('desc');
+            $table->bigInteger('game_id')->unsigned()->nullable();
+            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
+            $table->bigInteger('last_user_id')->unsigned();
+            $table->foreign('last_user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+};
