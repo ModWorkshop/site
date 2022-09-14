8f12843afdebcb994ea60f18bb08db1c44d04eb7
Luffy <luffydafloffi@gmail.com>
2022-09-14 23:05:27 +0300

    componnet for footer buttons

---
+++ components/layout/a-link-button.vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtLink class="link-button">
+        <slot/>
+    </NuxtLink>
+</template>
+
+<style scoped>
+.link-button {
+    color: var(--text-color);
+}
+
+.link-button:hover {
+    cursor: pointer;
+    color: var(--primary-hover-color);
+}
+</style>
