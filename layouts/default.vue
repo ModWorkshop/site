84923b56d95983854885392c277854196c931a68
Luffy <luffydafloffi@gmail.com>
2022-09-18 11:37:32 +0300

    Reduce padding for layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,22 +18,22 @@ const toasts = useState('toasts', () => []);
 </script>
 
 <style>
-    .layout {
-        min-height: 100vh;
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: auto 1fr auto;
-        grid-template-areas: 
-            'header'
-            'main'
-            'footer';
-    }
+.layout {
+    min-height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas: 
+        'header'
+        'main'
+        'footer';
+}
 
-    main {
-        grid-area: main;
-        display: flex;
-        flex-direction: column;
-        padding: 1rem;
-        align-items: center;
-    }
+main {
+    grid-area: main;
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+    align-items: center;
+}
 </style>
