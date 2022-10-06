4cb1d3b6099e7bf1d4e82f22c3db8ddf252515b2
Luffy <luffydafloffi@gmail.com>
2022-10-06 23:59:20 +0300

    /me route that redirect you to your profile

---
+++ pages/me.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useStore } from '~~/store';
+
+const router = useRouter();
+const { user } = useStore();
+
+definePageMeta({
+    middleware: 'users-only'
+});
+
+await router.replace(`user/${user.unique_name}`);
+</script>
