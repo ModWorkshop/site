6c0e5c4a0ab0a56a054ee4fa435623a00e51f911
Luffy <luffydafloffi@gmail.com>
2022-10-07 19:44:41 +0300

    GitLab sign in

--- components/site/the-social-logins.vue
+++ components/site/the-social-logins.vue
@@ -2,6 +2,7 @@
     <flex>
         <a-button :to="`${config.apiUrl}/auth/steam/redirect`" :icon="['fab', 'steam']" icon-size="lg"/>
         <a-button :to="`${config.apiUrl}/auth/github/redirect`" :icon="['fab', 'github']" icon-size="lg"/>
+        <a-button :to="`${config.apiUrl}/auth/gitlab/redirect`" :icon="['fab', 'gitlab']" icon-size="lg"/>
         <a-button :to="`${config.apiUrl}/auth/discord/redirect`" :icon="['fab', 'discord']" icon-size="lg"/>
     </flex>
 </template>
