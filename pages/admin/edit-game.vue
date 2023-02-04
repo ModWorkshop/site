0f9ab8a72f7b9f0323012f19daac66c741861d94
Luffy <luffydafloffi@gmail.com>
2023-02-04 16:36:51 +0200

    Fix admin game creation page
    Fixed it being basically broken
    Fixed it showing navbars when it shouldn't

--- pages/admin/edit-game.vue
+++ pages/admin/edit-game.vue
@@ -1,35 +1,34 @@
 <template>
-    <simple-resource-form v-model="currentGame" :can-save="canSaveOverride" :merge-params="mergeParams" url="games" redirect-to="/admin/games" @submit="submit">
-        <img-uploader id="thumbnail" v-model="thumbnailBlob" :label="$t('thumbnail')" :src="game.thumbnail">
+    <simple-resource-form v-model="vmGame" :can-save="canSaveOverride" :merge-params="mergeParams" url="games" redirect-to="/admin/games" @submit="submit">
+        <img-uploader id="thumbnail" v-model="thumbnailBlob" :label="$t('thumbnail')" :src="vmGame.thumbnail">
             <template #label="{ src }">
                 <game-thumbnail :src="src" style="height: 250px;"/>
             </template>
         </img-uploader>
-        <img-uploader id="banner" v-model="bannerBlob" :label="$t('banner')" :src="game.banner">
+        <img-uploader id="banner" v-model="bannerBlob" :label="$t('banner')" :src="vmGame.banner">
             <template #label="{ src }">
                 <a-banner :src="src" url-prefix="games/banners"/>
             </template>
         </img-uploader>
-        <a-input v-model="game.name" :label="$t('name')"/>
-        <a-input v-model="game.short_name" :label="$t('short_name')"/>
-        <a-input v-model="game.buttons" :label="$t('game_buttons')"/>
-        <a-input v-model="game.webhook_url" :label="$t('webhook_url')" desc="Whenever a new mod is published to this category, the site will call this webhook (generally Discord)"/>
+        <a-input v-model="vmGame.name" :label="$t('name')"/>
+        <a-input v-model="vmGame.short_name" :label="$t('short_name')"/>
+        <a-input v-model="vmGame.buttons" :label="$t('game_buttons')"/>
+        <a-input v-model="vmGame.webhook_url" :label="$t('webhook_url')" desc="Whenever a new mod is published to this category, the site will call this webhook (generally Discord)"/>
     </simple-resource-form>
 </template>
 
 <script setup lang="ts">
 import { Game } from '~~/types/models';
 
-const thumbnailBlob = ref(null);
-const bannerBlob = ref(null);
+const thumbnailBlob = ref();
+const bannerBlob = ref();
 const canSaveOverride = computed(() => !!(thumbnailBlob.value || bannerBlob.value));
 
 const props = defineProps<{
     game: Game
 }>();
 
-const currentGame = ref<Game>();
-watch(() => props.game, () => currentGame.value = props.game);
+const vmGame = useVModel(props, 'game');
 
 useNeedsPermission('manage-game', props.game);
 
--- pages/admin/game.vue
+++ pages/admin/game.vue
@@ -1,33 +1,32 @@
 <template>
-    <page-block :size="game.id ? 'md' : 'sm'" :game="game">
+    <page-block :size="game?.id ? 'md' : 'sm'" :game="game">
         <content-block class="p-6">
             <h3 v-if="game.id">
                 {{$t('game_name_settings', { game: game.name })}}
             </h3>
-            <a-nav side :root="`/admin/games/${id}`">
-                <template v-if="game.id">
-                    <h3>{{$t('general')}}</h3>
-                    <a-nav-link to="" :title="$t('home')"/>
-                    <a-nav-link v-if="hasPermission('manage-game', game)" to="settings" :title="$t('settings')"/>
-                    <a-nav-link v-if="hasPermission('manage-roles', game)" to="roles" :title="$t('roles')"/>
-                    <h3 class="mt-2">{{$t('moderation')}}</h3>
-                    <a-nav-link v-if="moderateUsers" to="cases" :title="$t('cases')"/>
-                    <a-nav-link v-if="moderateUsers" to="approvals" :title="$t('approvals')"/>
-                    <a-nav-link v-if="moderateUsers" to="bans" :title="$t('bans')"/>
-                    <a-nav-link v-if="manageMods" to="suspensions" :title="$t('suspensions')"/>
-                    <a-nav-link v-if="moderateUsers" to="reports" :title="$t('reports')"/>
-                    <h3 class="mt-2">{{$t('content')}}</h3>
-                    <a-nav-link v-if="manageMods" to="mods" :title="$t('mods')"/>
-                    <a-nav-link v-if="hasPermission('manage-tags', game)" to="tags" :title="$t('tags')"/>
-                    <a-nav-link v-if="hasPermission('manage-docs', game)" to="docs" :title="$t('docs')"/>
-                    <a-nav-link v-if="hasPermission('manage-categories', game)" to="categories" :title="$t('categories')"/>
-                    <a-nav-link v-if="hasPermission('manage-forum-categories', game)" to="forum-categories" :title="$t('forum_categories')"/>
-                    <a-nav-link v-if="hasPermission('manage-instructions', game)" to="instructions-templates" :title="$t('instructions_templates')"/>
-                </template>
+            <a-nav v-if="game?.id" side :root="`/admin/games/${id}`">
+                <h3>{{$t('general')}}</h3>
+                <a-nav-link to="" :title="$t('home')"/>
+                <a-nav-link v-if="hasPermission('manage-game', game)" to="settings" :title="$t('settings')"/>
+                <a-nav-link v-if="hasPermission('manage-roles', game)" to="roles" :title="$t('roles')"/>
+                <h3 class="mt-2">{{$t('moderation')}}</h3>
+                <a-nav-link v-if="moderateUsers" to="cases" :title="$t('cases')"/>
+                <a-nav-link v-if="moderateUsers" to="approvals" :title="$t('approvals')"/>
+                <a-nav-link v-if="moderateUsers" to="bans" :title="$t('bans')"/>
+                <a-nav-link v-if="manageMods" to="suspensions" :title="$t('suspensions')"/>
+                <a-nav-link v-if="moderateUsers" to="reports" :title="$t('reports')"/>
+                <h3 class="mt-2">{{$t('content')}}</h3>
+                <a-nav-link v-if="manageMods" to="mods" :title="$t('mods')"/>
+                <a-nav-link v-if="hasPermission('manage-tags', game)" to="tags" :title="$t('tags')"/>
+                <a-nav-link v-if="hasPermission('manage-docs', game)" to="docs" :title="$t('docs')"/>
+                <a-nav-link v-if="hasPermission('manage-categories', game)" to="categories" :title="$t('categories')"/>
+                <a-nav-link v-if="hasPermission('manage-forum-categories', game)" to="forum-categories" :title="$t('forum_categories')"/>
+                <a-nav-link v-if="hasPermission('manage-instructions', game)" to="instructions-templates" :title="$t('instructions_templates')"/>
                 <template #content>
                     <NuxtPage :game="game"/>
                 </template>
             </a-nav>
+            <NuxtPage v-else :game="game"/>
         </content-block>
     </page-block>
 </template>
