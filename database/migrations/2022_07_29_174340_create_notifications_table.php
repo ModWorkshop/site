ffe29df956fa0270008b21f872c9f84dd06bc0e4
Luffy <luffydafloffi@gmail.com>
2023-02-12 21:26:26 +0200

    Make context nullable for notifications

--- database/migrations/2022_07_29_174340_create_notifications_table.php
+++ database/migrations/2022_07_29_174340_create_notifications_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->morphs('notifiable'); //Any model that can be notified for. For example: Mod, Comment, Thread.
-            $table->morphs('context'); //Context for the notification. "You got a reply in a comment (notifiable) from user (context).
+            $table->nullableMorphs('context'); //Context for the notification. "You got a reply in a comment (notifiable) from user (context).
             $table->tinyText('type'); //Type of notification, reply_comment.
             $table->boolean('seen')->default(false);
             $table->json('data')->nullable(); //Extra context if necessary, hopefully not.
