6dba0dc19449cf612c75db1781fbd5f4783417b5
Luffy <luffydafloffi@gmail.com>
2023-02-12 19:34:01 +0200

    Mark useEditrResource as non-nullable/defined

--- composables/useEditResource.ts
+++ composables/useEditResource.ts
@@ -2,7 +2,7 @@ import { Ref } from 'nuxt/dist/app/compat/capi';
 import { SearchParams } from "ohmyfetch";
 import { useI18n } from "vue-i18n";
 
-export default async function<T>(name: string, url: string, template: T|null=null, params: SearchParams|null=null) {
+export default async function<T>(name: string, url: string, template: T|null=null, params?: SearchParams) {
     const route = useRoute();
     const { t } = useI18n();
 
@@ -16,9 +16,17 @@ export default async function<T>(name: string, url: string, template: T|null=nul
         const { error } = res;
 
         useHandleError(error, {
-            404: t('error_404')
+            404: t('error_resource_404')
         });
 
-        return res;
+        return {
+            refresh: res.refresh,
+            error: res.error,
+            // This gets handled, in this function we DO NOT expect a null/undefined result
+            // If it does return that, then there's something wrong.
+            // This function clearly returns either a template or errors in case something goes wrong.
+            // Why? So I don't have to write if if if a million times in places I don't expect it to be null...
+            data: res.data as Ref<T>
+        };
     }
 }
