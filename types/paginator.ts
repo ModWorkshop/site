efa7ea8b948739967c891fc93da22b27f537978a
Luffy <luffydafloffi@gmail.com>
2023-02-12 19:45:01 +0200

    Adjust Paginator type

--- types/paginator.ts
+++ types/paginator.ts
@@ -1,12 +1,29 @@
+export type PageMeta = {
+    current_page: number;
+    from: number;
+    last_page: number;
+    per_page: number;
+    to: number;
+    total: number;
+}
+
 export class Paginator<T = unknown> {
-    data: Array<T>;
-    meta: {
-        current_page: number;
-        from: number;
-        last_page: number;
-        path: number;
-        per_page: number;
-        to: number;
-        total: number;
+    constructor(data: T[]|null=null, meta: PageMeta|null = null) {
+        if (data) {
+            this.data = data;
+        }
+        if (meta) {
+            this.meta = meta;
+        }
     }
+
+    data: T[] = [];
+    meta: PageMeta = {
+        current_page: 1,
+        from: 0,
+        to: 1,
+        last_page: 1,
+        per_page: 0,
+        total: 0
+    };
 }
