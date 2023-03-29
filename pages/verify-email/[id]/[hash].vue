1668cd72c4a811a44e205548c726b08bab540c1a
Luffyyy <luffydafloffi@gmail.com>
2023-03-29 20:22:01 +0300

    Improve verify page

--- pages/verify-email/[id]/[hash].vue
+++ pages/verify-email/[id]/[hash].vue
@@ -2,13 +2,11 @@
     <page-block :column="false">
         <flex column class="mx-auto">
             <h2 class="mx-auto">{{$t(verifying ? 'verifying_email' : 'verified_email')}}</h2>
-            <a-loading v-if="verifying"/>
-            <a-button v-else class="mx-auto" to="/">{{$t('back_to_home')}}</a-button>
+            <a-button :loading="verifying" class="mx-auto" to="/">{{$t('back_to_home')}}</a-button>
         </flex>
     </page-block>
 </template>
 <script setup lang="ts">
-import { DateTime } from 'luxon';
 import { FetchError } from 'ofetch';
 import { useStore } from '~~/store';
 
@@ -29,7 +27,7 @@ if (process.client && user) {
     try {
         await useGet(`/email/verify/${route.params.id}/${route.params.hash}`, { params: route.query});
         verifying.value = false;
-        user.email_verified_at = DateTime.now().toISODate();
+        user.activated = true;
     } catch (e) {
         if (e instanceof FetchError && e.response) {
             showError({ statusCode: e.response.status, statusMessage: e.response.statusText });
