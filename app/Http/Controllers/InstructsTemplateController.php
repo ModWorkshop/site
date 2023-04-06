8860fa7fb6d80c4dd23b004aa72443950a114514
Luffyyy <luffydafloffi@gmail.com>
2023-04-06 08:09:27 +0300

    Attempt to fix not being able to create intstructions templates

--- app/Http/Controllers/InstructsTemplateController.php
+++ app/Http/Controllers/InstructsTemplateController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class InstructsTemplateController extends Controller
 {
     public function __construct() {
-        $this->authorizeResource(InstructsTemplate::class, 'instructs_template');
+        $this->authorizeGameResource(InstructsTemplate::class, 'instructs_template');
     }
     /**
      * Display a listing of the resource.
