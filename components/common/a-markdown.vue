edba4ef6471880355be38af2708e007f5a28db1e
Luffyyy <luffydafloffi@gmail.com>
2023-04-09 19:46:24 +0300

    Add padding to markdown component

--- components/common/a-markdown.vue
+++ components/common/a-markdown.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="markdown" v-html="parseMarkdown(text)"/>
+    <div class="markdown p-2" v-html="parseMarkdown(text)"/>
 </template>
 
 <script setup lang="ts">
 import { parseMarkdown } from "~~/utils/md-parser";
-defineProps({
-    text: String        
-});
+
+defineProps<{
+    text: string        
+}>();
 </script>
