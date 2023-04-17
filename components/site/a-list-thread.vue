078b2009220c1c1d69f46374996274535d32f6dd
Luffyyy <luffydafloffi@gmail.com>
2023-04-17 20:18:25 +0300

    fix: make threads clickable on lists

--- components/site/a-list-thread.vue
+++ components/site/a-list-thread.vue
@@ -1,18 +1,18 @@
 <template>
-    <tr class="cursor-pointer content-block thread" @click.self="clickThread(thread)">
-        <td>
+    <tr class="cursor-pointer content-block thread">
+        <td @click.self="clickThread(thread)">
             <a-icon v-if="!noPins && thread.pinned_at" style="transform: rotate(-45deg);" class="mr-2" icon="thumbtack"/>
             <NuxtLink :to="`/thread/${thread.id}`">{{thread.name}}</NuxtLink>
         </td>
-        <td><a-user :user="thread.user" @click.stop/></td>
-        <td v-if="!noCategory">
+        <td @click.self="clickThread(thread)"><a-user :user="thread.user" @click.stop/></td>
+        <td v-if="!noCategory" @click.self="clickThread(thread)">
             <NuxtLink v-if="thread.category" :to="to">{{thread.category.emoji}} {{thread.category.name}}</NuxtLink>
             <span v-else>-</span>
         </td>
-        <td>{{ thread.comment_count }}</td>
-        <td><time-ago :time="thread.bumped_at"/></td>
-        <td v-if="thread.last_user"><a-user :user="thread.last_user" @click.stop/></td>
-        <td v-else>{{$t('none')}}</td>
+        <td @click.self="clickThread(thread)">{{ thread.comment_count }}</td>
+        <td @click.self="clickThread(thread)"><time-ago :time="thread.bumped_at"/></td>
+        <td v-if="thread.last_user" @click.self="clickThread(thread)"><a-user :user="thread.last_user" @click.stop/></td>
+        <td v-else @click.self="clickThread(thread)">{{$t('none')}}</td>
     </tr>
 </template>
 
@@ -40,6 +40,8 @@ const to = computed(() => {
 });
 
 function clickThread(thread: Thread) {
+    console.log('...');
+    
     router.push(`/thread/${thread.id}`);
 }
 </script>
