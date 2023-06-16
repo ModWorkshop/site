d7f341dbe27ca2f06346b75284f7261052b77d30
Luffyyy <luffydafloffi@gmail.com>
2023-06-16 05:34:31 +0300

    fix: dependencies not available on editor mod page

--- root/components/pages/edit-mod/edit-mod-deps.vue
+++ root/components/pages/edit-mod/edit-mod-deps.vue
@@ -1,7 +1,7 @@
 <template>
     <a-select v-model="mod.instructs_template_id" :label="$t('instructions_template')" :url="`games/${mod.game_id}/instructs-templates`" clearable/>
     <md-editor v-model="mod.instructions" :label="$t('instructions')" rows="12"/>
-    <edit-dependencies :dependable="mod" url="mods"/>
+    <edit-mod-edit-dependencies :dependable="mod" url="mods"/>
 </template>
 
 <script setup lang="ts">
