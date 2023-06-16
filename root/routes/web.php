fb38880ac220b5964fa36790f754ab0f7f269c39
Luffyyy <luffydafloffi@gmail.com>
2023-06-16 18:16:18 +0300

    try to make docs appear on production

--- root/routes/web.php
+++ root/routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 /**
  * @hideFromAPIDocumentation
  */
+
+
+if (app()->environment('production')) {
+    $prefix = config('scribe.laravel.docs_url', '/');
+    $middleware = config('scribe.laravel.middleware', []);
+
+    Route::middleware($middleware)->group(function () use ($prefix) {
+        Route::view($prefix, 'scribe.index')->name('scribe');
+
+        Route::get("$prefix.postman", function () {
+            return new JsonResponse(Storage::disk('local')->get('scribe/collection.json'), json: true);
+        })->name('scribe.postman');
+
+        Route::get("$prefix.openapi", function () {
+            return response()->file(Storage::disk('local')->path('scribe/openapi.yaml'));
+        })->name('scribe.openapi');
+    });
+}
