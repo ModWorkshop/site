9fe28bc1df8402ec7c92f536e088a135edc56dd4
Luffyyy <luffydafloffi@gmail.com>
2023-06-22 16:10:24 +0300

    fix: add enthusiastgaming policy

--- root/pages/cookies.vue
+++ root/pages/cookies.vue
@@ -49,7 +49,7 @@
             </flex>
             <h2>{{$t('third_party_cookies')}}</h2>
             {{$t('third_party_cookies_desc')}}
-            <NuxtLink>Some random ad provider's policy</NuxtLink>
+            <NuxtLink to="https://www.enthusiastgaming.com/privacy-policy/">EnthusiastGaming Policy</NuxtLink>
         </content-block>
     </page-block>
 </template>
