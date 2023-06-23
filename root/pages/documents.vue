74e9a85d8c3747839c72a0455916a61ab92d048e
Luffyyy <luffydafloffi@gmail.com>
2023-06-23 19:36:08 +0300

    fix: users seeing global docs in game docs

--- root/pages/documents.vue
+++ root/pages/documents.vue
@@ -2,7 +2,7 @@
     <page-block :game="game" :breadcrumb="game ? breadcrumb : undefined" size="sm">
         <h2>{{$t('documents')}}</h2>
         <content-block>
-            <a-list url="documents" query :item-link="item => `${url}/${item.url_name}`" :params="{ game_id: game?.id }"/>
+            <a-list :url="apiUrl" query :item-link="item => `${url}/${item.url_name}`" :params="{ game_id: game?.id }"/>
         </content-block>
     </page-block>
 </template>
@@ -20,6 +20,7 @@ const { data: game } = await useResource<Game>('game', 'games');
 store.setGame(game.value);
 
 const url = computed(() => game.value ? `/g/${game.value.short_name}/document` : '/document');
+const apiUrl = computed(() => getGameResourceUrl('documents', game.value));
 
 const breadcrumb = computed(() => {
     return [
