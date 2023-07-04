656c423ee4840e7ccbb3075498ab97b6a608b9f1
Luffyyy <luffydafloffi@gmail.com>
2023-07-04 17:11:40 +0300

    fix: not being able to delete or mark as read some notifications

--- root/components/site/a-notification.vue
+++ root/components/site/a-notification.vue
@@ -31,8 +31,8 @@
             <time-ago :time="notification.created_at"/>
         </flex>
         <flex class="ml-auto my-auto">
-            <a-button v-if="!notification.seen" icon="ic:baseline-remove-red-eye" :title="$t('mark_as_read')" @click.stop="markAsSeen"/>
-            <a-button icon="mdi:trash" color="danger" :title="$t('delete')" @click.stop="deleteNotification()"/>
+            <a-button v-if="!notification.seen" icon="ic:baseline-remove-red-eye" :title="$t('mark_as_read')" @click.prevent="markAsSeen"/>
+            <a-button icon="mdi:trash" color="danger" :title="$t('delete')" @click.prevent="deleteNotification()"/>
         </flex>
     </component>
 </template>
