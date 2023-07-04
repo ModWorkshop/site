e1e6603131e54a70142aab1862e4ecfca1b4e36d
Luffyyy <luffydafloffi@gmail.com>
2023-07-04 21:13:42 +0300

    feat: best_match sorting for mods

--- root/app/Services/ModService.php
+++ root/app/Services/ModService.php
@@ -138,11 +138,14 @@ class ModService {
         $user = Auth::user();
 
         $sortBy = $val['sort'] ?? 'bumped_at';
+        $name = $val['query'];
 
         if ($sortBy === 'random') {
             $query->orderByRaw('RANDOM()');
         } else if ($sortBy === 'name') {
             $query->orderBy('name');
+        } else if ($sortBy === 'best_match') {
+            $query->orderByRaw("lower(name) = lower(?) DESC, name ILIKE '%' || ? || '%' DESC, name % ? DESC", [$name, $name, $name]);
         } else {
             if ($sortBy === 'published_at') {
                 $query->orderByRaw('published_at DESC NULLS LAST');
