b0629cfdec25273ca7e4d701968c34900495925e
Luffyyy <luffydafloffi@gmail.com>
2023-07-04 21:18:25 +0300

    feat: redirect old find page from v2

---
+++ root/server/routes/find/mod.ts
@@ -0,0 +1,10 @@
+//Redirects old mydownloads.php links
+
+export default defineEventHandler(async event => {
+    const query = getQuery(event);
+
+    const game = query.gid ? parseInt(query.gid as string) : '';
+    const q = query.q ? encodeURIComponent(query.q as string) : '';
+
+    return sendRedirect(event, `/search/mods?query=${q}&game=${game}`);
+});
