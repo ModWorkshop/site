21e88e2256570987beb8b487d802df6bf2e3bb17
Luffyyy <luffydafloffi@gmail.com>
2023-07-06 21:30:49 +0300

    return back to commit before for ads

--- root/plugins/load-ads.ts
+++ root/plugins/load-ads.ts
@@ -1,17 +1,18 @@
 
 
 export default defineNuxtPlugin((nuxtApp) => {
+    const launched = ref(false);
     nuxtApp.hook('page:finish', () => {
+        console.log('Page finished loading. Attempting to launch or reinstate ads.');
         if (process.client && (window.egAps && typeof(window.egAps.reinstate) === "function")) {
-            console.log(`Reinstate ads [URL: ${window.location.href}]`);
-            window.egAps.reinstate();
-        }
-    });
-
-    nuxtApp.hook('app:mounted', () => {
-        if (process.client && (window.egAps && typeof(window.egAps.reinstate) === "function")) {
-            console.log(`Launch ads`);
-            window.egAps.launchAds();
+            if (launched.value) { // Ads were not launched yet
+                console.log(`Reinstate ads [URL: ${window.location.href}]`);
+                window.egAps.reinstate();
+            } else { // Launch and set the ref so next time we only reinstate them.
+                console.log(`Launch ads`);
+                window.egAps.launchAds();
+                launched.value = true;
+            }
         }
     });
 })
