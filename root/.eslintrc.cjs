97b2f79d07278f86d845ebf84e111942b6066632
Luffyyy <luffydafloffi@gmail.com>
2023-08-27 11:06:32 +0300

    chore: remove deprecated eslint rule

--- root/.eslintrc.cjs
+++ root/.eslintrc.cjs
@@ -20,7 +20,6 @@ module.exports = {
 		'vue/multi-word-component-names': ['off'],
 		'vue/require-default-prop': ['off'],
 		'vue/no-mutating-props': ['off'],
-		'vue/no-setup-props-destructure': ['off'], // Vue 3.3 experimental feature allows this
 		"@typescript-eslint/no-non-null-assertion": ['off'], // Opinionated, if a programmer deems something to be non-nullable, then there is a reason for that.
 		'vue/no-v-html': ['off'], //While this rule has its point, we use it only in one place and that's the markdown parser which we sanitize
 		'vue/prefer-import-from-vue': ['off'] //Not really good as it DOES have some useful functions.
