0459697d34ea0bd7849b9c67e70a7b2e4bbeecda
Luffyyy <luffydafloffi@gmail.com>
2023-09-08 16:52:34 +0300

    try caching first then run it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/usr/sbin/crond -b
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-
 # Cache stuff for performance
 php artisan config:clear && php artisan config:cache
+
+/usr/sbin/crond -b
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
