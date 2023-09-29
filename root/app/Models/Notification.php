138c8aa0151758d2bb5ff2488dcd9148fcfc9017
Luffyyy <luffydafloffi@gmail.com>
2023-09-29 22:01:57 +0300

    fix: not being able to add members

--- root/app/Models/Notification.php
+++ root/app/Models/Notification.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * App\Models\Notification
