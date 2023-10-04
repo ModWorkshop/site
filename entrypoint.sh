c8314b2332e034092d6a6cf0c91244510ac00c75
Luffyyy <luffydafloffi@gmail.com>
2023-10-04 21:56:46 +0300

    try running sitemap generation after site starts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
 # Cache stuff for performance
+
+echo "Clear and cache config"
 php artisan config:clear && php artisan config:cache
 
+echo "Run crond + visord"
 /usr/sbin/crond -b
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
+
+echo "Generate sitemap"
+php artisan sitemap:generate
