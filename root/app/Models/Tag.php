3f1efc7405e5469a6d48b9245cfd4cb37d24c64f
Luffyyy <luffydafloffi@gmail.com>
2023-10-04 14:15:46 +0300

    perf: cache tags

--- root/app/Models/Tag.php
+++ root/app/Models/Tag.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Database\Factories\TagFactory;
 use Eloquent;
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -52,7 +53,7 @@ use Illuminate\Support\Carbon;
  */
 class Tag extends Model
 {
-    use HasFactory;
+    use HasFactory, Cachable;
 
     protected $hidden = ['pivot'];
 
