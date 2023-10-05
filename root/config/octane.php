8c0132c68c5a1ff88f2160458193dd33a4ec7423
Luffyyy <luffydafloffi@gmail.com>
2023-10-05 15:36:20 +0300

    chore: disable http compression in octane
    probably pointless?

--- root/config/octane.php
+++ root/config/octane.php
@@ -38,7 +38,7 @@ return [
 
     'swoole' => [
         'options' => [
-            'http_compression' => true,
+            'http_compression' => false,
             'http_compression_level' => 6, // 1 - 9
             'compression_min_length' => 20,
             'package_max_length' => 1500 * 1024 * 1024, // 20MB
