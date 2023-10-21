4097d104f9cedceb8d63f6a1994f286e548cef06
Luffyyy <luffydafloffi@gmail.com>
2023-10-21 17:16:30 +0300

    chore: remove unused route

--- root/routes/api.php
+++ root/routes/api.php
@@ -82,7 +82,6 @@ Route::patch('mods/{mod}/members/accept', [ModMemberController::class, 'accept']
 Route::resource('mods.members', ModMemberController::class)->only(['store', 'destroy', 'update']);
 Route::resource('mods.dependencies', ModDependencyController::class);
 Route::patch('mods/{mod}/owner/accept', [ModController::class, 'acceptTransferRequest']);
-Route::get('mods/followed', [ModController::class, 'followed']);
 Route::post('mods/{mod}/register-view', [ModController::class, 'registerView']);
 Route::post('mods/{mod}/register-download', [ModController::class, 'registerDownload']);
 Route::get('mods/waiting', [ModController::class, 'waiting']);
