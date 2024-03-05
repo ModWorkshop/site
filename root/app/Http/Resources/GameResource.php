4d74a09e70169caa93732a2493cded9cef7595a9
Luffyyy <luffydafloffi@gmail.com>
2024-03-05 03:57:20 +0200

    chore: make game resource manual

--- root/app/Http/Resources/GameResource.php
+++ root/app/Http/Resources/GameResource.php
@@ -11,9 +11,11 @@ use Auth;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\Request;
 use App\Http\Resources\BaseResource;
+use App\Models\Game;
 use Illuminate\Http\Resources\MissingValue;
 use JsonSerializable;
 
+/**@mixin Game */
 class GameResource extends BaseResource
 {
     /**
@@ -32,8 +34,28 @@ class GameResource extends BaseResource
 
         $isCurrent = APIService::currentGame()?->id === $this->id;
 
-        return array_merge(parent::toArray($request), [
-            'followed' => $this->whenLoaded('followed'),
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'short_name' => $this->short_name,
+            'disporder' => $this->disporder,
+            'thumbnail' => $this->thumbnail,
+            'banner' => $this->banner,
+            'buttons' => $this->buttons,
+            'last_date' => $this->last_date,
+            'mod_count' => $this->mod_count,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'forum_id' => $this->forum_id,
+            'default_mod_manager_id' => $this->default_mod_manager_id,
+
+            'mods' => $this->whenLoaded('mods'),
+            'tags' => $this->whenLoaded('tags'),
+            'roles' => $this->whenLoaded('roles'),
+            'reports' => $this->whenLoaded('reports'),
+            'reports' => $this->whenLoaded('reports'),
+            'mod_managers' => $this->whenLoaded('mod_managers'),
+
             'webhook_url' => $this->when($user?->hasPermission('manage-game', $this->resource), $this->webhook_url),
             'report_count' => $this->when($isCurrent && $moderateUsers, fn() => $this->reportCount),
             'waiting_count' => $this->when($isCurrent && $manageMods, fn() => $this->waitingCount),
@@ -41,6 +63,6 @@ class GameResource extends BaseResource
             'announcements' => $this->when($isCurrent, fn() => $this->announcements),
             'mods_count' => $this->whenCounted('viewableMods'),
             'mod_manager_ids' => $this->whenLoaded('modManagers', fn () => Arr::pluck($this->modManagers, 'id')),
-        ]);
+        ];
     }
 }
