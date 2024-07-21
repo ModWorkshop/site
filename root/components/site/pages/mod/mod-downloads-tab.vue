be2e1dcec40f2946bff52bce04e3fea28f884e60
Luffyyy <luffydafloffi@gmail.com>
2024-07-21 03:37:23 +0300

    Show when files and links are loaded properly

--- root/components/site/pages/mod/mod-downloads-tab.vue
+++ root/components/site/pages/mod/mod-downloads-tab.vue
@@ -1,12 +1,13 @@
 <template>
-    <m-flex column gap="6">
-        <m-list v-if="loadingFiles || files?.meta.total" v-model:page="filesPage" :limit="10" :title="$t('files')" :items="files" :loading="loadingFiles">
+    <m-loading v-if="loadingFiles || loadingLinks"/>
+    <m-flex v-else column gap="6">
+        <m-list v-if="files?.meta.total" v-model:page="filesPage" :limit="10" :title="$t('files')" :items="files">
             <template #item="{ item }">
                 <mod-download :file="item" :mod="mod" type="file"/>
             </template>
         </m-list>
 
-        <m-list v-if="loadingLinks || links?.meta.total" v-model:page="linksPage" :limit="10" :title="$t('links')" :items="links" :loading="loadingLinks">
+        <m-list v-if="links?.meta.total" v-model:page="linksPage" :limit="10" :title="$t('links')" :items="links">
             <template #item="{ item }">
                 <mod-download :file="item" :mod="mod" type="link"/>
             </template>
