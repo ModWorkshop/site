5d78cbfa073371754b0f8d5c665b1bb660aa79f3
Luffyyy <luffydafloffi@gmail.com>
2024-10-03 08:20:46 +0300

    Footer design change

--- root/components/site/pages/the-footer.vue
+++ root/components/site/pages/the-footer.vue
@@ -1,41 +1,43 @@
 <template>
     <footer>
-        <m-flex column gap="5">
-            <m-flex wrap gap="3">
-                <m-link @click="scrollToTop">{{$t('return_to_top')}}</m-link>
-                <m-link to="/document/rules">{{$t('rules')}}</m-link>
-                <m-link to="/about">{{$t('about')}}</m-link>
-                <m-link to="/document/terms">{{$t('terms')}}</m-link>
-                <m-link to="/document/impressum">{{$t('impressum')}}</m-link>
-                <m-link to="/document/policy">{{$t('privacy')}}</m-link>
-                <m-link to="/cookies">{{$t('cookie_policy')}}</m-link>
-                <m-link to="https://status.modworkshop.net">{{$t('status')}}</m-link>
-            </m-flex>
-            <m-flex column>
-                ModWorkshop {{ runtimeConfig.version }} ({{ commitHash }})
-                <span>
-                    <i18n-t keypath="made_with_love" scope="global">
-                        <template #luffy>
-                            <NuxtLink to="/user/Luffy">Luffy</NuxtLink>
+        <m-flex class="page-block-nm">
+            <m-flex column gap="5">
+                <m-flex wrap gap="3">
+                    <m-link @click="scrollToTop">{{$t('return_to_top')}}</m-link>
+                    <m-link to="/document/rules">{{$t('rules')}}</m-link>
+                    <m-link to="/about">{{$t('about')}}</m-link>
+                    <m-link to="/document/terms">{{$t('terms')}}</m-link>
+                    <m-link to="/document/impressum">{{$t('impressum')}}</m-link>
+                    <m-link to="/document/policy">{{$t('privacy')}}</m-link>
+                    <m-link to="/cookies">{{$t('cookie_policy')}}</m-link>
+                    <m-link to="https://status.modworkshop.net">{{$t('status')}}</m-link>
+                </m-flex>
+                <m-flex column>
+                    ModWorkshop {{ runtimeConfig.version }} ({{ commitHash }})
+                    <span>
+                        <i18n-t keypath="made_with_love" scope="global">
+                            <template #luffy>
+                                <NuxtLink to="/user/Luffy">Luffy</NuxtLink>
+                            </template>
+                        </i18n-t>
+                    </span>
+                </m-flex>
+                <m-flex class="items-center">
+                    <i18n-t keypath="operated_by" scope="global">
+                        <template #company>
+                            <m-img class="inline-block" src="milk_deluxe.webp" loading="lazy" is-asset width="40" height="24" alt="CompanyLogo"/> Milk Deluxe
                         </template>
                     </i18n-t>
-                </span>
+                </m-flex>
             </m-flex>
-            <m-flex class="items-center">
-                <i18n-t keypath="operated_by" scope="global">
-                    <template #company>
-                        <m-img class="inline-block" src="milk_deluxe.webp" loading="lazy" is-asset width="40" height="24" alt="CompanyLogo"/> Milk Deluxe
+            <m-flex class="lg:ml-auto mb-auto items-center">
+                <m-select v-model="store.colorScheme" style="width: 250px;" :options="colors">
+                    <template #any-option="{option}">
+                        <div class="circle" :style="{backgroundColor: `var(--mws-${option.id})`, marginTop: '2px'}"/> {{$t(`color_${option.id}`)}}
                     </template>
-                </i18n-t>
+                </m-select>
+                <m-select v-model="locale" default="en" :options="locales" :value-by="option => option.code"/>
             </m-flex>
-        </m-flex>
-        <m-flex class="lg:ml-auto mb-auto items-center">
-            <m-select v-model="store.colorScheme" style="width: 250px;" :options="colors">
-                <template #any-option="{option}">
-                    <div class="circle" :style="{backgroundColor: `var(--mws-${option.id})`, marginTop: '2px'}"/> {{$t(`color_${option.id}`)}}
-                </template>
-            </m-select>
-            <m-select v-model="locale" default="en" :options="locales" :value-by="option => option.code"/>
         </m-flex>
     </footer>
 </template>
@@ -91,10 +93,13 @@ function scrollToTop() {
 <style>
 footer {
     max-width: 100vw;
-    margin-top: 8px;
-    padding: 1.5rem 2rem;
+    margin-top: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
     gap: 24px;
     display: flex;
+    flex-direction: column;
+    align-items: center;
     background-color: var(--header-footer-color);
     grid-area: footer;
 }
