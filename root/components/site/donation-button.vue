6532108e1fd7bbc35886f6a7215cc12cbfed1f48
Luffyyy <luffydafloffi@gmail.com>
2024-10-15 04:33:50 +0300

    Github sponsor button

--- root/components/site/donation-button.vue
+++ root/components/site/donation-button.vue
@@ -1,5 +1,12 @@
 <template>
-    <NuxtLink v-if="link" :to="link">
+    <iframe 
+        v-if="link && type == 'github'"
+        :src="`https://github.com/sponsors/${link.match(github)![1]}/button`"
+        height="32"
+        width="114"
+        style="border: 0; border-radius: 6px;"
+    />
+    <NuxtLink v-else-if="link" :to="link">
         <m-img :src="image" class="donation-button" is-asset/>
     </NuxtLink>
 </template>
@@ -15,21 +22,47 @@ const kofi = /(?:https:\/\/)?(?:www\.)?ko-fi\.com\/(\w+)/;
 const paypalme  = /(?:https:\/\/)?(?:www\.)?paypal\.me\/(\w+)/;
 const paypalBtn = /(?:https:\/\/)?(?:www\.)?paypal(?:\.me|\.com)\/donate\/\?hosted_button_id=(\w+)/;
 const paypal = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+const github = /(?:https:\/\/)?(?:www\.)?github\.com\/sponsors\/(\w+)/;
 
-const link = computed(() => {
+const type = computed(() => {
     const l = props.link;
     if (l) {
         if (kofi.test(l)) {
-            return `https://ko-fi.com/${l.match(kofi)![1]}`;
+            return 'kofi';
         } else if (bmc.test(l)) {
-            return `https://buymeacoffee.com/${l.match(bmc)![1]}`;
+            return 'bmc';
         } else if (paypalme.test(l)) {
-            return `https://paypal.me/${l.match(paypalme)![1]}`;
+            return 'paypalme';
         } else if (paypal.test(l)) {
-            return `https://www.paypal.com/donate/?business=${l}`;
+            return 'paypal';
         } else if (paypalBtn.test(l)) {
-            return `https://www.paypal.com/donate/?hosted_button_id=${l.match(paypalBtn)![1]}`;
+            return 'paypalBtn';
+        } else if (github.test(l)) {
+            return 'github';
         }
+    }
+});
+
+const link = computed(() => {
+    const l = props.link;
+
+    if (!l) {
+        return null;
+    }
+
+    switch(type.value) {
+        case 'kofi':
+            return `https://ko-fi.com/${l.match(kofi)![1]}`;
+        case 'bmc':
+            return `https://buymeacoffee.com/${l.match(bmc)![1]}`;
+        case 'paypalme':
+            return `https://paypal.me/${l.match(paypalme)![1]}`;
+        case 'paypal':
+            return `https://www.paypal.com/donate/?business=${l}`;
+        case 'paypalBtn':
+            return `https://www.paypal.com/donate/?hosted_button_id=${l.match(paypalBtn)![1]}`;
+        case 'github':
+            return `https://github.com/sponsors/${l.match(github)![1]}`;
     }
 });
 
