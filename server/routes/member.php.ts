d5324e2e09c5016479ea448d1004ee451415fe8c
Luffyyy <luffydafloffi@gmail.com>
2024-10-20 03:29:05 +0300

    Add member.php migration

---
+++ server/routes/member.php.ts
@@ -0,0 +1,12 @@
+//Redirects old mydownloads.php links
+
+export default defineEventHandler(async event => {
+    const query = getQuery(event);
+
+    if (query.action === 'profile' && query.uid) {
+        const uid = parseInt(query.uid as string);
+        return sendRedirect(event, `user/${uid}`);
+    }
+
+    return setResponseStatus(event, 404);
+});
