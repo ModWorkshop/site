977783e9d5e2a685ce3cb19b50dea04ed171eab0
Luffyyy <luffydafloffi@gmail.com>
2024-10-22 01:35:59 +0300

    Footer changes
    More buttons and better look

--- components/site/pages/the-footer.vue
+++ components/site/pages/the-footer.vue
@@ -1,48 +1,51 @@
 <template>
     <footer>
-        <m-flex class="page-block-nm max-lg:flex-col p-4" gap="3">
-            <m-flex column gap="5">
-                <m-flex wrap gap="3">
-                    <m-link @click="scrollToTop">{{$t('return_to_top')}}</m-link>
-                    <m-link to="/document/rules">{{$t('rules')}}</m-link>
-                    <m-link to="/about">{{$t('about')}}</m-link>
-                    <m-link to="/document/terms">{{$t('terms')}}</m-link>
-                    <m-link to="/document/impressum">{{$t('impressum')}}</m-link>
-                    <m-link to="/document/policy">{{$t('privacy')}}</m-link>
-                    <m-link to="/cookies">{{$t('cookie_policy')}}</m-link>
-                    <m-link to="https://status.modworkshop.net">{{$t('status')}}</m-link>
+        <m-flex column class="p-4 footer-content" gap="8">
+            <div class="footer-links">
+                <m-flex class="items-center mb-auto footer-customize">
+                    <m-select v-model="store.colorScheme" style="width: 220px;" :options="colors">
+                        <template #any-option="{option}">
+                            <div class="circle" :style="{backgroundColor: `var(--mws-${option.id})`, marginTop: '2px'}"/> {{$t(`color_${option.id}`)}}
+                        </template>
+                    </m-select>
+                    <m-select v-model="locale" default="en" :options="locales" :value-by="option => option.code"/>
                 </m-flex>
-                <m-flex class="text-2xl" gap="3">
-                    <m-link to="https://x.com/ModWorkshop"><i-ri-twitter-x-fill/></m-link>
+                <m-flex column gap="4" style="grid-area: pages;">
+                    <m-link @click="scrollToTop">{{$t('return_to_top')}}</m-link>
+                    <m-link to="/support">{{$t('support_us')}}</m-link>
+                    <m-link to="/about">{{$t('about')}}</m-link>
+                    <m-link to="/document/rules">{{$t('rules')}}</m-link>
+                </m-flex>
+                <m-flex column gap="4" style="grid-area: sites;">
+                    <m-link to="https://translate.modworkshop.net/">{{$t('translation_site')}}</m-link>
+                    <m-link to="https://status.modworkshop.net">{{$t('status')}}</m-link>
+                    <m-link to="https://wiki.modworkshop.net/">{{$t('wiki')}}</m-link>
+                    <m-link :to="runtimeConfig.apiUrl">{{$t('api')}}</m-link>
+                </m-flex>
+                <m-flex column gap="4" style="grid-area: legal;">
+                    <m-link to="/document/terms">{{$t('terms')}}</m-link>
+                    <m-link to="/cookies">{{$t('cookie_policy')}}</m-link>
+                    <m-link to="/document/policy">{{$t('privacy')}}</m-link>
+                    <m-link to="/document/impressum">{{$t('impressum')}}</m-link>
+                </m-flex>
+            </div>
+
+            <m-flex gap="3" class="flex-1 items-center place-content-center" wrap>
+                <m-flex column gap="2">
+                    <span>
+                        ModWorkshop {{ runtimeConfig.version }} ({{ commitHash }}) Made With ❤ By <NuxtLink to="/user/Luffy">Luffy</NuxtLink>
+                    </span>
+                    <m-flex class="items-center">
+                        Operated By <m-img class="inline-block" src="milk_deluxe.webp" loading="lazy" is-asset width="40" height="24" alt="CompanyLogo"/> Milk Deluxe
+                    </m-flex>
+                </m-flex>
+                <m-flex class="sm:ml-auto text-2xl" gap="3">
                     <m-link to="https://discord.gg/Eear4JW"><i-ri-discord-fill/></m-link>
+                    <m-link to="https://x.com/ModWorkshop"><i-ri-twitter-x-fill/></m-link>
+                    <m-link to="https://bsky.app/profile/modworkshop.bsky.social"><i-ri-bluesky-fill/></m-link>
                     <m-link to="https://www.youtube.com/@modworkshop-yt"><i-ri-youtube-fill/></m-link>
                     <m-link to="https://github.com/ModWorkshop"><i-ri-github-fill/></m-link>
                 </m-flex>
-                <m-flex column>
-                    ModWorkshop {{ runtimeConfig.version }} ({{ commitHash }})
-                    <span>
-                        <i18n-t keypath="made_with_love" scope="global">
-                            <template #luffy>
-                                <NuxtLink to="/user/Luffy">Luffy</NuxtLink>
-                            </template>
-                        </i18n-t>
-                    </span>
-                </m-flex>
-                <m-flex class="items-center">
-                    <i18n-t keypath="operated_by" scope="global">
-                        <template #company>
-                            <m-img class="inline-block" src="milk_deluxe.webp" loading="lazy" is-asset width="40" height="24" alt="CompanyLogo"/> Milk Deluxe
-                        </template>
-                    </i18n-t>
-                </m-flex>
-            </m-flex>
-            <m-flex class="lg:ml-auto mb-auto items-center">
-                <m-select v-model="store.colorScheme" style="width: 250px;" :options="colors">
-                    <template #any-option="{option}">
-                        <div class="circle" :style="{backgroundColor: `var(--mws-${option.id})`, marginTop: '2px'}"/> {{$t(`color_${option.id}`)}}
-                    </template>
-                </m-select>
-                <m-select v-model="locale" default="en" :options="locales" :value-by="option => option.code"/>
             </m-flex>
         </m-flex>
     </footer>
@@ -97,6 +100,47 @@ function scrollToTop() {
 </script>
 
 <style>
+.footer-links {
+    display: grid;
+    gap: 1rem;
+    grid-template-areas:
+        "customize pages sites legal";
+    grid-template-columns: minmax(0, 3fr) repeat(2, minmax(0, 1fr));
+}
+
+.footer-customize {
+    grid-area: customize;
+    margin-right: auto;
+}
+
+.footer-content {
+    width: 75%;
+}
+
+@media (max-width:1280px) {
+    .footer-content {
+        width: 85%;
+    }
+}
+
+@media (max-width:900px) {
+    .footer-links {
+        grid-template-areas:
+            "customize customize customize"
+            "pages sites legal";
+        grid-template-columns:
+            repeat(3, minmax(0, 1fr));
+    }
+    
+    .footer-customize {
+        margin-left: auto;
+    }
+
+    .footer-content {
+        width: 90%;
+    }
+}
+
 footer {
     max-width: 100vw;
     margin-top: 1rem;
@@ -108,11 +152,5 @@ footer {
     align-items: center;
     background-color: var(--header-footer-color);
     grid-area: footer;
-}
-
-@media (max-width:768px) {
-    footer {
-        flex-direction: column-reverse;
-    }
 }
 </style>
