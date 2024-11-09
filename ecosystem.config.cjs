ef2ad160985e186e95507efd3c21a4ab316f1ef4
Luffy <luffydafloffi@gmail.com>
2024-11-09 14:06:06 +0200

    Fixed PM2 not running with max instances

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
             port: '3000',
             exec_mode: 'cluster',
             max_memory_restart: "1G",
-            instances: process.env.NODE_ENV === "prod" ? "max" : 1,
+            instances: process.env.NODE_ENV === "production" ? "max" : 1,
             script: './.output/server/index.mjs'
         }
     ]
