308abea27fdb6d47e8eae28afe76547d36d2f173
Luffy <luffydafloffi@gmail.com>
2025-01-29 00:42:15 +0200

    Upgrade to Nuxt 3.15.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
 			siteUrl: '',
 			storageUrl: '',
 			hcaptchaSiteKey: '',
-			version: '3.5.1',
+			version: '3.5.2',
 			presignedUpload: false,
 			commitHash: ''
 		},
