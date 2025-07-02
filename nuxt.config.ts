a24681eaf24d11b8f850b7439debca2635cb77a6
Luffy <luffydafloffi@gmail.com>
2025-07-02 19:03:09 +0300

    Fix version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
 			siteUrl: '',
 			storageUrl: '',
 			hcaptchaSiteKey: '',
-			version: '3.5.4',
+			version: '3.5.3',
 			presignedUpload: false,
 			commitHash: ''
 		},
