16cb44334e5d563b8c23202bb1b40c2297dcc2b6
Luffy <luffydafloffi@gmail.com>
2026-05-05 18:04:57 +0300

    Some adjustments to entrypoint.sh
    > https://stackoverflow.com/questions/32255814/what-purpose-does-using-exec-in-docker-entrypoint-scripts-serve
    + Caching routes there instead in docker compose

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,5 +5,8 @@
 echo "Clear and cache config"
 php artisan config:clear && php artisan config:cache
 
+echo "Clear and cache route"
+php artisan route:clear && php artisan route:cache
+
 echo "Run supervisord"
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
