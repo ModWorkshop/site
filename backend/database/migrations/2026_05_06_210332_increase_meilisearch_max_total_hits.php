fbda8a38034c356422d6b479b1e1da6702214932
Luffy <luffydafloffi@gmail.com>
2026-05-07 22:51:28 +0300

    Set maxTotalHits to 50k
    While this may not be the best I decided to go with it, it doesn't seem to really affect the performance as much, but I might be wrong.
    If it becomes an issue we should find a good solution that allows us to still have the whole pagination.
    
    From the docs of meilisearch, it doesn't seem like this is done for performance reasons, but as a protection against scalpers which we don't really care for? Feels weird to put that as a reason, performance reasons would be far more alarming than scalpers.

--- backend/database/migrations/2026_05_06_210332_increase_meilisearch_max_total_hits.php
+++ backend/database/migrations/2026_05_06_210332_increase_meilisearch_max_total_hits.php
@@ -15,7 +15,7 @@ return new class extends Migration
         $index = $client->index('mods');
 
         // If anyone knows a better way I'm all ears, but from quick testing there's no effect and we DO want to get a correct total results
-        $index->updatePagination(['maxTotalHits' => 100000]);
+        $index->updatePagination(['maxTotalHits' => 50000]);
     }
 
     /**
@@ -23,6 +23,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        $client = new \Meilisearch\Client('http://meilisearch:7700', env('MEILISEARCH_KEY'));
+        $index = $client->index('mods');
+
+        $index->updatePagination(['maxTotalHits' => 1000]);
     }
 };
