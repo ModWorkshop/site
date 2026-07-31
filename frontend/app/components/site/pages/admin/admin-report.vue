8f85f596419abee2808a0431a17cf85c7e06aa4a
Luffy <luffydafloffi@gmail.com>
2026-08-01 01:01:22 +0300

    Fix some overflow issues

--- frontend/app/components/site/pages/admin/admin-report.vue
+++ frontend/app/components/site/pages/admin/admin-report.vue
@@ -1,21 +1,21 @@
 <template>
-	<m-flex class="list-button">
-		<m-flex column :class="{ archived: report.archived }">
+	<m-flex class="list-button" gap="3">
+		<m-flex column :class="{ archived: report.archived, 'w-full': true, 'wrap-break-word': true }">
 			<m-tag class="capitalize mr-auto">{{ report.reportable_type }}</m-tag>
-			<m-flex class="items-center">{{ $t('reported_by') }}: <a-user :user="report.user" avatar-size="xs"/> <m-time :datetime="report.created_at" relative/></m-flex>
+			<m-flex class="items-center" wrap>{{ $t('reported_by') }}: <a-user :user="report.user" avatar-size="xs"/> <m-time :datetime="report.created_at" relative/></m-flex>
 			<m-flex class="items-center">{{ $t('reported_user') }}: <a-user :user="report.reported_user ?? reportedUser" avatar-size="xs"/></m-flex>
 			<span>{{ $t('reason') }}:</span>
 			<blockquote>{{ report.reason }}</blockquote>
 			<span>{{ contentTitle }}: <span v-if="content && currentContent != content">{{ $t('edited') }}</span></span>
 			<details>{{ content }}</details>
 		</m-flex>
-		<m-flex column class="ml-auto my-auto" gap="2">
-			<m-flex class="ml-auto">
+		<m-flex column wrap class="ml-auto my-auto max-lg:flex-row">
+			<m-flex class="ml-auto" wrap>
 				<m-button v-if="report.reportable?.user_id" :to="`/admin/${casesUrl}?user=${report.reportable.user_id}`">{{ $t('warn_owner') }}</m-button>
 				<mod-suspend v-if="report.reportable_type == 'mod' && report.reportable" :mod="report.reportable as Mod"/>
 				<m-button v-if="reportLink" :to="reportLink">{{ $t('go_to_content') }}</m-button>
 			</m-flex>
-			<m-flex class="ml-auto">
+			<m-flex class="lg:ml-auto">
 				<m-button v-if="report.archived" color="danger" @click="deleteReport"><i-mdi-delete/> {{ $t('delete') }}</m-button>
 				<m-button style="opacity: 1;" @click="toggleArchiveReport">{{ $t(report.archived ? 'unarchive' : 'archive') }}</m-button>
 			</m-flex>
